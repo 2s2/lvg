d51144c33330cf52c0d0daa5baded86c2c8302a4
2s2 <2flap2@gmail.com>
2020-03-15 00:04:29 +0200

    REF: post footer moved

--- wp-content/themes/twentysixteen/template-parts/content-search.php
+++ wp-content/themes/twentysixteen/template-parts/content-search.php
@@ -10,6 +10,13 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php twentysixteen_post_thumbnail(); ?>
+
+	<?php twentysixteen_excerpt(); ?>
 	<?php if ( 'post' === get_post_type() ) : ?>
 
 		<footer class="entry-footer">
@@ -43,12 +50,5 @@
 
 	<?php endif; ?>
 	
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header><!-- .entry-header -->
-
-	<?php twentysixteen_post_thumbnail(); ?>
-
-	<?php twentysixteen_excerpt(); ?>
 </article><!-- #post-## -->
 
