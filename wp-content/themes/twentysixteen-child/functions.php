5699986cfcde9e28727e35851ca22ea2c6614c1b
2s2 <2flap2@gmail.com>
2020-03-15 02:39:38 +0200

    ADD: Remove 'Category' word from title

--- wp-content/themes/twentysixteen-child/functions.php
+++ wp-content/themes/twentysixteen-child/functions.php
@@ -4,3 +4,16 @@
  * Remove <br> and <p> tags for posts
  */
 remove_filter( 'the_content', 'wpautop' );
+
+/**
+ * Remove 'Category' word from title
+ */
+add_filter( 'get_the_archive_title', 'artabr_remove_name_cat' );
+function artabr_remove_name_cat( $title ){
+	if ( is_category() ) {
+		$title = single_cat_title( '', false );
+	} elseif ( is_tag() ) {
+		$title = single_tag_title( '', false );
+	}
+	return $title;
+}
